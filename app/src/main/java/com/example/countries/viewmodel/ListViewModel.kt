493f160c191c3f9b4423d00337c0b6bb36e4da85
Kijan <kijan3@gmail.com>
2020-10-06 08:38:23 +0200

    views

--- app/src/main/java/com/example/countries/viewmodel/ListViewModel.kt
+++ app/src/main/java/com/example/countries/viewmodel/ListViewModel.kt
@@ -1,8 +1,36 @@
 package com.example.countries.viewmodel
 
+import android.arch.lifecycle.MutableLiveData
 import android.arch.lifecycle.ViewModel
+import com.example.countries.model.Country
 
 class ListViewModel: ViewModel() {
+
+    val countries = MutableLiveData<List<Country>>()
+    val countryLoadError = MutableLiveData<Boolean>()
+    val loading = MutableLiveData<Boolean>()
+
+    fun refresh(){
+        fetchCountries()
+    }
+
+    private fun fetchCountries(){
+
+        val mockData = listOf(Country("Polska"),
+            Country("Anglia"),
+            Country("Francja"),
+            Country("Węgry"),
+            Country("Czechy"),
+            Country("Słowacja"),
+            Country("Chorwacja"),
+            Country("Grecja")
+        )
+
+        countryLoadError.value = false
+        loading.value = false
+        countries.value = mockData
+
+    }
 
 
 }
